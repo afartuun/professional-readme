badcdd676617126755f483e5bbd4f33fe392a768
Fartuun Ali <fartuunali@Fartuuns-MacBook-Pro.local>
2022-07-18 20:16:56 -0500

    final commit

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -11,7 +11,13 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license){
+    console.log(license)
+    return `https://img.shields.io/badge/License-${license}-blue.svg`
+  }
+  }
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -33,6 +39,7 @@ function generateMarkdown(data) {
 
   ## License 
   ${data.license}
+  #### ${renderLicenseLink(data.license)} 
 
   ## Contributing
   ${data.credts}
